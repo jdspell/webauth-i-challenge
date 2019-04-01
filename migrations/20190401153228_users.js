b5e2de50dd99e7d758b8a6017e7efd83f496678a
jdspell <spelljd15@gmail.com>
2019-04-01 18:07:41 -0400

    lost commit history as I did not initially fork and incorrectly cloned when trying to fix it

---
+++ migrations/20190401153228_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(tbl){
+      tbl.increments();
+
+      tbl
+        .string('username', 256)
+        .notNullable();
+    
+      tbl
+        .string('password', 256)
+        .notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
